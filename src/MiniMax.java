f04ea99b699811e39e3a0594f0e546d93fb6b769
Matthew Brown <mattb1892@gmail.com>
2014-12-11 15:40:09 -0500

    MiniMax is set up

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+
 /**
  * Created by matt on 12/8/2014.
  */
 public class MiniMax {
 
-    public static void main(String[] args) {
+    private LineGame game;
 
+    public MiniMax(LineGame game) {
+        this.game = game;
     }
+
+    public int staticEvaluator() {
+        return 0;
+    }
+
+    public ArrayList<int[]> getValidMoves(int row) {
+        return game.getValidMovesPerRow(row);
+    }
+
+    public void play() {
+        
+    }
+
 }
